746103b6ca5c026b21b8f03970b9b8b80e43fdc8
Romario Hagemann <romariosascha@gmail.com>
2019-11-16 15:03:57 +0100

    JS function Karten anzeigen

--- public/script.js
+++ public/script.js
@@ -11,3 +11,26 @@ function getCards() {
         showCards(data);
     })
 };
+function showCards(cards) {
+    var cardsSection = document.getElementById("list");
+    for(var i = 0; i< cards.length; i++){
+        var section = document.createElement("aside");
+        section.className += "suggestion";
+        var company = document.createElement("h4");
+        company.innerHTML = cards[i].company;
+        var customer = document.createElement("h5");
+        customer.innerHTML = cards[i].customer;
+        var username = document.createElement("h5");
+        username.innerHTML = cards[i].username;
+        var homepage = document.createElement('a');
+        var link = document.createTextNode("Homepage");
+        homepage.appendChild(link);
+        homepage.title = "Homepage der Firma";
+        homepage.href = homepage;
+        section.appendChild(company);
+        section.appendChild(customer);
+        section.appendChild(username);
+        section.appendChild(homepage);
+        cardsSection.appendChild(section);
+    }
+};
